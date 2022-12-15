efb319d5f7dac3e01e38e686a1559c9b2e8d135b
Abdulganiyu wareez <ganiyuwareez2017@gmail.com>
2022-12-15 00:10:36 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h
+/**
+* _isupper - checks is a character is uppercase
+* @c: The character to print
+* Return 1 if true, 0 if false
+*/
+int_isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
