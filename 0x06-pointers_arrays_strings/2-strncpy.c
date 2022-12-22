641c62993beb53cd22d6ac76938e0b7178698988
Abdulganiyu wareez <ganiyuwareez2017@gmail.com>
2022-12-22 05:27:35 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncpy - copy a string with n
+ * _strncpy - copys a string with n
  * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
  * Return: dest
  */
-char _strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
